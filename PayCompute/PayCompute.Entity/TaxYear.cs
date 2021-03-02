1c99a3da70e09548c594d458dc41b6de2a245801
Bao phung <b.giaphung@gmail.com>
2021-03-01 20:41:20 -0600

    adding project

---
+++ PayCompute/PayCompute.Entity/TaxYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayCompute.Entity
+{
+    public class TaxYear
+    {
+        public int ID { get; set; }
+        public string YearOfTax { get; set; }
+    }
+}
