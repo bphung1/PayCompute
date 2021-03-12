558e4a1e7597892849e8f3e99c30ee1d3aad495e
Bao phung <b.giaphung@gmail.com>
2021-03-12 13:33:38 -0600

    detail and delete action methods - EmployeeController

--- PayCompute/PayCompute/Controllers/EmployeeController.cs
+++ PayCompute/PayCompute/Controllers/EmployeeController.cs
@@ -74,7 +74,7 @@ namespace PayCompute.Controllers
 
                 if (model.ImageUrl != null && model.ImageUrl.Length > 0)
                 {
-                    await NewMethod(model.ImageUrl, employee);
+                    await getImgUrl(model.ImageUrl, employee);
                 }
                 await _employeeService.CreateAsync(employee);
                 return RedirectToAction(nameof(Index));
@@ -149,7 +149,7 @@ namespace PayCompute.Controllers
 
                 if (model.ImageUrl != null && model.ImageUrl.Length > 0)
                 {
-                    await NewMethod(model.ImageUrl, employee);
+                    await getImgUrl(model.ImageUrl, employee);
                 }
                 await _employeeService.UpdateAsync(employee);
                 return RedirectToAction(nameof(Index));
@@ -157,7 +157,62 @@ namespace PayCompute.Controllers
             return View();
         }
 
-        private async Task NewMethod(IFormFile imgUrl, Employee employee)
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            EmployeeDetailViewModel model = new EmployeeDetailViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                NationalInsuranceNo = employee.NationalInsuranceNo,
+                Phone = employee.Phone,
+                Email = employee.Email,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                ImageUrl = employee.ImageUrl,
+                Postcode = employee.Postcode
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var model = new EmployeeDeleteViewModel()
+            {
+                Id = employee.Id,
+                FullName = employee.FullName
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult >Delete(EmployeeDeleteViewModel model)
+        {
+            await _employeeService.Delete(model.Id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task getImgUrl(IFormFile imgUrl, Employee employee)
         {
             var uploadDir = @"images/employee";
             var fileName = Path.GetFileNameWithoutExtension(imgUrl.FileName);
