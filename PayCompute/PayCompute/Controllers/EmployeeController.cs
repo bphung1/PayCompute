a72948ab50d452702ba25a86131e4e0b65e5d9f9
Bao phung <b.giaphung@gmail.com>
2021-03-12 13:04:37 -0600

    implemented edit post and get methods + refactor some code

--- PayCompute/PayCompute/Controllers/EmployeeController.cs
+++ PayCompute/PayCompute/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PayCompute.Entity;
 using PayCompute.Models;
@@ -73,19 +74,99 @@ namespace PayCompute.Controllers
 
                 if (model.ImageUrl != null && model.ImageUrl.Length > 0)
                 {
-                    var uploadDir = @"images/employee";
-                    var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
-                    var extension = Path.GetExtension(model.ImageUrl.FileName);
-                    var webRootPath = _hostingEnvironment.WebRootPath;
-                    fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
-                    var path = Path.Combine(webRootPath, uploadDir, fileName);
-                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
-                    employee.ImageUrl = "/" + uploadDir + "/" + fileName;
+                    await NewMethod(model.ImageUrl, employee);
                 }
                 await _employeeService.CreateAsync(employee);
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = _employeeService.GetById(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EmployeeEditViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FirstName = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                LastName = employee.LastName,
+                Gender = employee.Gender,
+                Email = employee.Email,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                NationalInsuranceNo = employee.NationalInsuranceNo,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                Phone = employee.Phone,
+                Postcode = employee.Postcode,
+                Designation = employee.Designation,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = _employeeService.GetById(model.Id);
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+
+                employee.EmployeeNo = model.EmployeeNo;
+                employee.FirstName = model.FirstName;
+                employee.LastName = model.LastName;
+                employee.MiddleName = model.MiddleName;
+                employee.NationalInsuranceNo = model.NationalInsuranceNo;
+                employee.Gender = model.Gender;
+                employee.Email = model.Email;
+                employee.DOB = model.DOB;
+                employee.DateJoined = model.DateJoined;
+                employee.Phone = model.Phone;
+                employee.Designation = model.Designation;
+                employee.PaymentMethod = model.PaymentMethod;
+                employee.StudentLoan = model.StudentLoan;
+                employee.UnionMember = model.UnionMember;
+                employee.Address = model.Address;
+                employee.City = model.City;
+                employee.Postcode = model.Postcode;
+
+                if (model.ImageUrl != null && model.ImageUrl.Length > 0)
+                {
+                    await NewMethod(model.ImageUrl, employee);
+                }
+                await _employeeService.UpdateAsync(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        private async Task NewMethod(IFormFile imgUrl, Employee employee)
+        {
+            var uploadDir = @"images/employee";
+            var fileName = Path.GetFileNameWithoutExtension(imgUrl.FileName);
+            var extension = Path.GetExtension(imgUrl.FileName);
+            var webRootPath = _hostingEnvironment.WebRootPath;
+            fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
+            var path = Path.Combine(webRootPath, uploadDir, fileName);
+            await imgUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+            employee.ImageUrl = "/" + uploadDir + "/" + fileName;
         }
     }
 }
